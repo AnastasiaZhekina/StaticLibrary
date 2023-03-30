350b1533138b888ed9485b978bbe5e32e18e5c87
AnastasiaZhekina <yfcnzjkzdbnz09@gmail.com>
2023-03-30 13:09:00 +0500

    first

---
+++ Program/Program.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream> 
+#include <windows.h> 
+#include "../StaticLibrary/Creet.h" 
+using namespace std;
+
+
+int main() {
+
+	setlocale(LC_ALL, "rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	Creeter person;
+	person.greet();
+}
---
+++ StaticLibrary/Creet.cpp
@@ -0,0 +1,6 @@
+#include "Creet.h"
+
+void Creeter::greet() {
+	cout << "Введите имя: ";
+	cin >> name; cout << "Здравствуйте, " << name << "!";
+};
